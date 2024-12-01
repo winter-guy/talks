85219088b7b7f979d7b05d41815b1ae18113ab71
ranvir jha <tappers_callous0h@icloud.com>
2024-12-01 16:21:35 +0530

    #1 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { HlmAccordionDirective, HlmAccordionItemDirective, HlmAccordionTriggerDirective, HlmAccordionContentComponent, HlmAccordionIconDirective } from '@spartan-ng/ui-accordion-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    
-  `,
+  imports: [
+    HlmAccordionDirective,
+		HlmAccordionItemDirective,
+		HlmAccordionTriggerDirective,
+		HlmAccordionContentComponent,
+		HlmAccordionIconDirective,
+		HlmIconComponent,
+  ],
+  templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
