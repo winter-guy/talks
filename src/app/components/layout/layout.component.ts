72316ebdde07ddf1d2e192c88553e835a6ba144d
ranvir jha <tappers_callous0h@icloud.com>
2024-12-05 00:43:20 +0530

    general design change

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { HlmButtonModule } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent],
+  imports: [CommonModule, RouterModule, NavbarComponent, HlmButtonModule],
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent {}
